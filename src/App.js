80bc6d54d081414c8d7ff869ccb3b378e0ae520d
Marianna Dei <d.marianna@icloud.com>
2023-02-21 22:19:29 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Coded by {" "}
-        <a href="https://www.instagram.com/deimari/" target="_blank"> Marianna Dei</a>{" "}
-        and open-sourse code on {" "}
-        <a href="https://github.com/MariDei/react-weather-app" target="_blank"> GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://www.instagram.com/deimari/"
+            target="_blank"
+            rel="noopener noreferrer"
+          > Marianna Dei{" "}
+          </a> and {" "}
+          <a
+            href="https://github.com/MariDei/react-weather-app"
+            target="_blank"
+          > open-source code on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
