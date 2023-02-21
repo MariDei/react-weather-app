6d256453d614ff0552766abd1c6516f874936644
Marianna Dei <d.marianna@icloud.com>
2023-02-21 21:48:32 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        Coded by {" "}
+        <a href="https://www.instagram.com/deimari/" target="_blank"> Marianna Dei</a>{" "}
+        and open-sourse code on {" "}
+        <a href="https://github.com/MariDei/react-weather-app" target="_blank"> GitHub</a>
+      </footer>
     </div>
   );
 }
