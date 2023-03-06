d7061065ddbd88dabf65f3e48d179ec31eac22ab
Marianna Dei <d.marianna@icloud.com>
2023-03-06 22:12:03 +0200

    Updated WeatherTemperature.js + css + updated to mobile responsive

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,10 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("celsius");
-   
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function fahrenheit() {
-        return Math.round((props.celsius * 9) / 5 + 32);
-    }
-    
-    if(unit === "celsius") {
-        return (
-          <div className="WeatherTemperature">
-            <span className="temperature">{Math.round(props.celsius)}</span>
-            <span className="unit-active">
-              ℃ |
-              <a href="/" className="fahrenheit" onClick={showFahrenheit}>
-              ℉
-              </a>
-            </span>
-          </div>
-        );
-    } else {
-        return (
-          <div className="WeatherTemperature">
-            <span className="temperature">{fahrenheit()}</span>
-            <span className="unit-active">
-              <a href="/" className="celsius" onClick={showCelsius}>
-                {" "}
-                ℃
-              </a>{" "}|℉{" "}
-            </span>
-          </div>
-        );
-    }    
-}
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">℃</span>
+    </div>
+  );
+}    
